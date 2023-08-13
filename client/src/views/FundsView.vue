4d715152e36782b2953b208f166967668c58ac86
PauloScheuer <pauloroberto.scheuergomes.404@gmail.com>
2023-08-13 15:29:33 -0300

    feature: implements stocks page

--- client/src/views/FundsView.vue
+++ client/src/views/FundsView.vue
@@ -1,45 +1,23 @@
 <template>
   <div class="content-container">
-    <h2 class="title">Funds</h2>
-    <p class="text">In this page, you can take a look at Brazilian stock funds and their portfolio.</p>
-    <p class="text">Feel free to filter and order the results acording to you criteria.</p>
+    <PageHeader title="Funds"
+      :text="['In this page, you can take a look at Brazilian stock funds and their portfolio.', 'Feel free to filter and order the results acording to you criteria.']" />
     <h3 class="subtitle">List of funds:</h3>
-    <div class="fund-search">
-      <input type="text" placeholder="Search for a CNPJ" class="fund-search-input" @input="handleChangeFilter" />
-      <div class="fund-search-order">
-        <span class="fund-search-order-title">Order:</span>
-        <VueFeather :type="tempSearchParams.orderBy > 0 ? 'chevron-up' : 'chevron-down'" class="fund-search-order-icon"
-          @click="tempSearchParams.orderBy *= -1" />
-      </div>
-      <VueFeather type="search" class="fund-search-button" @click="fetchInitialData()" />
-    </div>
-    <div class="fund" v-for="(fund, index) in funds">
-      <div class="fund-header">
-        <p class="fund-title">
-          {{ fund.fund }} - {{ fund.stocks.length }} stocks
-        </p>
-        <VueFeather size="24" :type="fund.isOpen ? 'chevron-up' : 'chevron-down'" class="fund-expand"
-          @click="handleExpand(index)" />
-      </div>
-      <div class="fund-content" :class="fund.isOpen && 'fund-content-open'">
-        <div class="fund-content-inner">
-          <p v-for="stock in fund.stocks" class="fund-item">{{ stock }}</p>
-        </div>
-      </div>
-    </div>
-    <div class="fund-content-more" @click="loadMoreFunds()">
-      <VueFeather size="32" type="plus" />
-    </div>
+    <SearchBar :order-by="tempSearchParams.orderBy" @change-filter="handleChangeFilter" @change-order="handleChangeOrder"
+      @search="fetchInitialData" />
+    <ListItems sublabel="stocks" :items="items" @request-more-items="loadMoreFunds()" />
   </div>
 </template>
 <script lang="ts">
+import ListItems from '@/components/ListItems.vue';
+import PageHeader from '@/components/PageHeader.vue';
+import SearchBar from '@/components/SearchBar.vue';
 import api from '@/services/api';
-import VueFeather from 'vue-feather'
+import VueFeather from 'vue-feather';
 
 type Fund = {
   fund: string,
-  stocks: string[],
-  isOpen: boolean
+  stocks: string[]
 }
 
 export default {
@@ -70,12 +48,7 @@ export default {
         }
       });
       if (res.status === 200) {
-        this.funds = res.data.map((fund: Fund) => {
-          return {
-            ...fund,
-            isOpen: false
-          }
-        });
+        this.funds = res.data;
       }
     },
     async loadMoreFunds() {
@@ -89,128 +62,28 @@ export default {
       });
 
       if (res.status === 200) {
-        const returnedFunds = res.data.map((fund: Fund) => {
-          return {
-            ...fund,
-            isOpen: false
-          }
-        });
+        const returnedFunds = res.data;
         this.funds = [...this.funds, ...returnedFunds];
       }
     },
-    handleExpand(nIndex: number) {
-      this.funds[nIndex].isOpen = !this.funds[nIndex].isOpen
-    },
     handleChangeFilter(event: Event) {
       this.tempSearchParams.filterBy = (event.target as HTMLInputElement).value
+    },
+    handleChangeOrder() {
+      this.tempSearchParams.orderBy *= -1;
     }
   },
-  components: { VueFeather }
+  computed: {
+    items() {
+      return this.funds.map(item => {
+        return {
+          key: item.fund,
+          list: item.stocks
+        }
+      })
+    }
+  },
+  components: { VueFeather, PageHeader, SearchBar, ListItems }
 }
 </script>
-<style scoped>
-.fund {
-  background-color: white;
-  margin-bottom: 8px;
-  border-radius: 5px;
-  padding: 8px;
-  width: 500px;
-}
-
-.fund-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.fund-title {
-  font-size: 20px;
-  font-weight: 500;
-}
-
-.fund-expand {
-  cursor: pointer;
-  transition: color 0.5s;
-}
-
-.fund-expand:hover {
-  color: #2ECC71;
-}
-
-.fund-content {
-  display: grid;
-  grid-template-rows: 0fr;
-  transition: grid-template-rows 0.5s;
-}
-
-.fund-content-open {
-  grid-template-rows: 1fr;
-}
-
-.fund-content-inner {
-  overflow: hidden;
-}
-
-.fund-item {
-  font-size: 14px;
-  margin-bottom: 4px;
-}
-
-.fund-content-more {
-  background-color: white;
-  border-radius: 5px;
-  cursor: pointer;
-  transition: color 0.5s;
-  width: 500px;
-  padding: 0px 8px;
-  text-align: center;
-}
-
-.fund-content-more:hover {
-  color: #2ECC71;
-}
-
-.fund-search {
-  background-color: white;
-  width: 500px;
-  padding: 8px;
-  border-radius: 5px;
-  margin-bottom: 24px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.fund-search-input {
-  background-color: #f0f0f0;
-  border: none;
-  padding: 8px;
-  border-radius: 5px;
-  font-size: 14px;
-}
-
-.fund-search-input:focus {
-  outline: none;
-}
-
-.fund-search-order {
-  display: flex;
-  align-items: center;
-}
-
-.fund-search-order-title {
-  font-weight: bold;
-  font-size: 14px;
-}
-
-.fund-search-order-icon,
-.fund-search-button {
-  cursor: pointer;
-  transition: color 0.5s;
-}
-
-.fund-search-order-icon:hover,
-.fund-search-button:hover {
-  color: #2ECC71;
-}
-</style>
+<style scoped></style>
--- client/src/views/StocksView.vue
+++ client/src/views/StocksView.vue
@@ -1,4 +1,89 @@
-<template></template>
-<script setup lang="ts">
+<template>
+  <div class="content-container">
+    <PageHeader title="Stocks"
+      :text="['In this page, you can take a look at Brazilian stocks and the funds that contain them on their portfolio.', 'Feel free to filter and order the results acording to you criteria.']" />
+    <h3 class="subtitle">List of stocks:</h3>
+    <SearchBar :order-by="tempSearchParams.orderBy" @change-filter="handleChangeFilter" @change-order="handleChangeOrder"
+      @search="fetchInitialData" />
+    <ListItems sublabel="funds" :items="items" @request-more-items="loadMoreStocks()" />
+  </div>
+</template>
+<script lang="ts">
+import ListItems from '@/components/ListItems.vue';
+import PageHeader from '@/components/PageHeader.vue';
+import SearchBar from '@/components/SearchBar.vue';
+import api from '@/services/api';
+import VueFeather from 'vue-feather';
+
+type Stock = {
+  stock: string,
+  funds: string[]
+}
+
+export default {
+  data() {
+    return {
+      stocks: [] as Stock[],
+      curPage: 1,
+      tempSearchParams: {
+        filterBy: '',
+        orderBy: 1
+      },
+      searchParams: {
+        filterBy: '',
+        orderBy: 1
+      }
+    }
+  },
+  mounted() {
+    this.fetchInitialData();
+  },
+  methods: {
+    async fetchInitialData() {
+      this.searchParams = { ...this.tempSearchParams };
+      const res = await api.get('/relationships/stocks', {
+        params: {
+          filterBy: this.searchParams.filterBy,
+          orderBy: this.searchParams.orderBy
+        }
+      });
+      if (res.status === 200) {
+        this.stocks = res.data;
+      }
+    },
+    async loadMoreStocks() {
+      this.curPage++;
+      const res = await api.get('/relationships/stocks', {
+        params: {
+          filterBy: this.searchParams.filterBy,
+          orderBy: this.searchParams.orderBy,
+          pageNumber: this.curPage,
+        }
+      });
+
+      if (res.status === 200) {
+        const returnedStocks = res.data;
+        this.stocks = [...this.stocks, ...returnedStocks];
+      }
+    },
+    handleChangeFilter(event: Event) {
+      this.tempSearchParams.filterBy = (event.target as HTMLInputElement).value
+    },
+    handleChangeOrder() {
+      this.tempSearchParams.orderBy *= -1;
+    }
+  },
+  computed: {
+    items() {
+      return this.stocks.map(item => {
+        return {
+          key: item.stock,
+          list: item.funds
+        }
+      })
+    }
+  },
+  components: { VueFeather, PageHeader, SearchBar, ListItems }
+}
 </script>
-<style></style>
+<style scoped></style>
